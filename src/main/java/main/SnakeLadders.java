1f74b3bfd914e4246ff6d22354d49a7a2c554e1e
SHARMI <SHARMI@DEVI-PC>
2019-03-15 20:10:16 +0530

    My Snakes and ladders Project

--- src/main/java/main/SnakeLadders.java
+++ src/main/java/main/SnakeLadders.java
@@ -12,17 +12,26 @@ public class SnakeLadders{
 	}
 	public static boolean snake(int i,int j) {
 		//snake is in 4th place thats the co-ordinate [1,2] i.e, i=1 & j=2
-		//write your code here
+		if (i == 1 && j == 2)
+		{
+			return true;
+		}
 		return false;
 	}
 	public static boolean ladder(int i,int j) {
 		//ladder is in 3rd place thats the co-ordinate [0,3] i.e, i=0 and j=2
-		//write your code here
+		if (i == 0 && j == 2)
+		{
+			return true;
+		}
 		return false;
 	}
 	public static boolean gameOver(int score) {
 		//if the score is greater than 9 you win. 
-		//write your code here
+		if (score > 9)
+		{
+			return true;
+		}
 		return false;
 	}
 	public static void main(String args[]) {
@@ -43,7 +52,8 @@ public class SnakeLadders{
 		
 		System.out.println("Enter 'y' to roll the dice");
 		char y = sc.next().charAt(0);
-		int dice=-1;
+		int dice;
+		dice = -1;
 		if(y=='y')
 			dice = random();
 		int place = 0;
@@ -51,7 +61,8 @@ public class SnakeLadders{
 		System.out.println(dice);
 	
 		while(true) {
-			int play = place;
+			int play;
+			play = place;
 			for(int i=0;i<3;i++) {
 				for(int j=0;j<3;j++) {
 					play --;
